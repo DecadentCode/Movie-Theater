9510cb5dbc8b0ee1ae3278bdffc6e93cfef325da
godoshian <94014358+godoshian@users.noreply.github.com>
2022-05-23 10:53:05 -0400

    boot

---
+++ theater-backend/src/main/java/com/example/theaterbackend/TheaterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.theaterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheaterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheaterBackendApplication.class, args);
+	}
+
+}
